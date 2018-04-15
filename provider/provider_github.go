83bde619fff0c7f65c8fb220e20c593439c4e605
sven klemmer <svenklemmer@aol.com>
2018-04-15 11:57:13 +0200

    remove context support as i need more time to look in it

--- provider/provider_github.go
+++ provider/provider_github.go
@@ -2,9 +2,9 @@ package provider
 
 import (
 	"github.com/google/go-github/github"
-	"context"
 	"net/http"
 	"strings"
+	"context"
 )
 
 var client *github.Client
@@ -31,14 +31,14 @@ func NewGithubOptions(repoStr string) (*GithubOptions) {
 }
 
 func NewGithubProvider(options *GithubOptions) (*GithubProvider) {
-	if options.ctx == nil {
-		options.ctx = context.Background()
-	}
+	//if options != nil && options.GitOptions != nil && options.GitOptions.ctx == nil {
+	//	options.GitOptions.ctx = context.TODO()
+	//}
 	return &GithubProvider{options}
 }
 
 func (gp *GithubProvider) GetLatestRelease() (*Release, error) {
-	rr, _, err := getClient(gp.options).Repositories.GetLatestRelease(gp.options.ctx, gp.options.Owner, gp.options.Repo)
+	rr, _, err := getClient(gp.options).Repositories.GetLatestRelease(context.TODO(), gp.options.Owner, gp.options.Repo)
 	if err != nil {
 		return nil, err
 	}
@@ -49,7 +49,7 @@ func (gp *GithubProvider) GetLatestRelease() (*Release, error) {
 }
 
 func (gp *GithubProvider) GetBinary(release *Release) (*Release, error) {
-	ra, _, _ := getClient(gp.options).Repositories.GetReleaseAsset(gp.options.ctx, gp.options.Owner, gp.options.Repo, release.CommitID)
+	ra, _, _ := getClient(gp.options).Repositories.GetReleaseAsset(context.TODO(), gp.options.Owner, gp.options.Repo, release.CommitID)
 	release.DownloadUrl = ra.GetBrowserDownloadURL()
 	return downloadBinary(release)
 }
--- provider/provider_github_test.go
+++ provider/provider_github_test.go
@@ -25,7 +25,7 @@ func TestNewGithubOptions(t *testing.T) {
 func TestGithubProvider_GetLatestRelease(t *testing.T) {
 	expected := &Release{Version: "0.0.1", CommitID: 10547150}
 
-	github := NewGithubProvider(&GithubOptions{GitOptions: &GitOptions{Owner: "sklemmer", Repo: "go-autoupdate-example"}})
+	github := NewGithubProvider(NewGithubOptions("sklemmer/go-autoupdate-example"))
 	release, err := github.GetLatestRelease()
 	assert.NoError(t, err)
 	assert.Equal(t, expected, release)
@@ -34,7 +34,7 @@ func TestGithubProvider_GetLatestRelease(t *testing.T) {
 func TestGithubProvider_GetBinary(t *testing.T) {
 	expected := &Release{Version: "0.0.1", CommitID: 10547150}
 
-	github := NewGithubProvider(&GithubOptions{GitOptions: &GitOptions{Owner: "sklemmer", Repo: "go-autoupdate-example"}})
+	github := NewGithubProvider(NewGithubOptions("sklemmer/go-autoupdate-example"))
 	release, err := github.GetLatestRelease()
 	assert.NoError(t, err)
 	assert.Equal(t, expected, release)
@@ -44,7 +44,7 @@ func TestGithubProvider_GetBinary(t *testing.T) {
 func TestGithubProvider_GetBinary_NoDownloads(t *testing.T) {
 	expected := &Release{Version: "0.0.1", CommitID: 10547150}
 
-	github := NewGithubProvider(&GithubOptions{GitOptions: &GitOptions{Owner: "sklemmer", Repo: "go-autoupdate-example"}})
+	github := NewGithubProvider(NewGithubOptions("sklemmer/go-autoupdate-example"))
 	_, err := github.GetBinary(expected)
 
 	assert.Error(t, err, errNoDownloadUrlFound)
